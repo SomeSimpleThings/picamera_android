1294e85c41581e121b7eb1092c4e1e883de0a477
SomeSimpleThings <makesomethingsimple@gmail.com>
2021-02-03 21:27:38 +0300

    connecting graphql

--- app/src/main/java/com/simplethings/picamera/MainActivity.kt
+++ app/src/main/java/com/simplethings/picamera/MainActivity.kt
@@ -2,10 +2,40 @@ package com.simplethings.picamera
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.apollographql.apollo.ApolloCall
+import com.apollographql.apollo.api.Response
+import com.apollographql.apollo.exception.ApolloException
+import com.simplethings.picamera.app.src.main.apollo.com.simplethings.picamera.GetPhotosQuery
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getPhotos()
+    }
+
+    private fun getPhotos() {
+        val client = NetworkService.getInstance()
+            ?.getApolloClient()
+
+        val photos = GetPhotosQuery()
+
+        client
+            ?.query(photos)
+            ?.enqueue(object : ApolloCall.Callback<GetPhotosQuery.Data>() {
+
+                override fun onResponse(response: Response<GetPhotosQuery.Data>) {
+                    if (!response.hasErrors()) {
+                        runOnUiThread(Runnable {
+                            findViewById<TextView>(R.id.text_view).text =
+                                response.data?.photos?.get(0).toString()
+                        })
+
+                    }
+                }
+
+                override fun onFailure(e: ApolloException) {}
+            })
     }
 }
